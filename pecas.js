3da04d1b0fd9bab1ec6fd634d84c42226528aa34
Lidia Panis <lpse@live.com>
2021-09-21 19:48:31 -0300

    Lista de peças

---
+++ pecas.js
@@ -0,0 +1,30 @@
+var listadepecas = ["peca1", "peca2", "peca3"];
+
+console.log("Quantidade de caracteres");
+
+if (listadepecas.length <= 10) {
+  console.log("As peças podem ser cadastradas");
+}
+
+console.log("Quantidade de caracteres");
+
+for (let index = 0; index < listadepecas.length; index++) {
+  const pecaAtual = listadepecas[index];
+  if (pecaAtual.length < 3) {
+    console.log(
+      pecaAtual +
+        ": a peça possui nome inferior a 3 caracteres e não pode ser cadastrada"
+    );
+  } else {
+    console.log(pecaAtual + ": a peça pode ser cadastrada");
+  }
+}
+
+console.log("peso da peca");
+const pesodapecaemgr = 50;
+
+if (pesodapecaemgr >= 100) {
+  console.log("peso suficiente");
+} else {
+  console.log("peso insuficiente");
+}
